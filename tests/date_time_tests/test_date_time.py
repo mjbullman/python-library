4e0a30cb6cfb458c3e9b74c4fe017d7b20716458
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-04 08:57:06 +0300

    Testing code updates

--- tests/date_time_tests/test_date_time.py
+++ tests/date_time_tests/test_date_time.py
@@ -1,4 +1,4 @@
-from src.python_library import date_time
+from src.python_library.date_times.date_time import convert_to_24_hour_format
 
 
 class TestDateTime:
@@ -7,20 +7,20 @@ class TestDateTime:
         """
         Test conversion of valid 12-hour format time strings to 24-hour format.
         """
-        assert date_time.convert_to_24_hour_format('07:05:45PM') == '19:05:45'
-        assert date_time.convert_to_24_hour_format('12:40:22AM') == '00:40:22'
-        assert date_time.convert_to_24_hour_format('12:40:22PM') == '12:40:22'
-        assert date_time.convert_to_24_hour_format('01:05:45AM') == '01:05:45'
+        assert convert_to_24_hour_format('07:05:45PM') == '19:05:45'
+        assert convert_to_24_hour_format('12:40:22AM') == '00:40:22'
+        assert convert_to_24_hour_format('12:40:22PM') == '12:40:22'
+        assert convert_to_24_hour_format('01:05:45AM') == '01:05:45'
 
         # edge cases.
-        assert date_time.convert_to_24_hour_format('12:00:00PM') == '12:00:00'
-        assert date_time.convert_to_24_hour_format('12:00:00AM') == '00:00:00'
+        assert convert_to_24_hour_format('12:00:00PM') == '12:00:00'
+        assert convert_to_24_hour_format('12:00:00AM') == '00:00:00'
 
     def test_convert_to_24_hour_format_invalid_data(self):
         """
         Test handling of invalid 12-hour format time strings.
         """
-        assert date_time.convert_to_24_hour_format('') is False
-        assert date_time.convert_to_24_hour_format('01:05:45AMPM') is False
-        assert date_time.convert_to_24_hour_format('24:01:01AM') is False
-        assert date_time.convert_to_24_hour_format('11:60:60PM') is False
+        assert convert_to_24_hour_format('') is False
+        assert convert_to_24_hour_format('01:05:45AMPM') is False
+        assert convert_to_24_hour_format('24:01:01AM') is False
+        assert convert_to_24_hour_format('11:60:60PM') is False
--- tests/sorting_algorithms_tests/test_sorting_algorithms.py
+++ tests/sorting_algorithms_tests/test_sorting_algorithms.py
@@ -1,22 +1,22 @@
-from src.python_library import sorting_algorithms
+from src.python_library.sorting_algorithms.sorting import bubble_sort
 
 
 class TestBubbleSortAlgorithm:
     def test_unsorted_list(self):
-        assert sorting_algorithms.bubble_sort([64, 34, 25, 12, 22, 11, 90]) == [11, 12, 22, 25, 34, 64, 90]
+        assert bubble_sort([64, 34, 25, 12, 22, 11, 90]) == [11, 12, 22, 25, 34, 64, 90]
 
     def test_empty_list(self):
-        assert sorting_algorithms.bubble_sort([]) == []
+        assert bubble_sort([]) == []
 
     def test_single_element_list(self):
-        assert sorting_algorithms.bubble_sort([1]) == [1]
+        assert bubble_sort([1]) == [1]
 
     def test_already_sorted_list(self):
-        assert sorting_algorithms.bubble_sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+        assert bubble_sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
 
     def test_reverse_sorted_list(self):
-        assert sorting_algorithms.bubble_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+        assert bubble_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
 
     def test_list_with_duplicates(self):
-        assert sorting_algorithms.bubble_sort([3, 1, 2, 1, 3, 2, 1]) == [1, 1, 1, 2, 2, 3, 3]
+        assert bubble_sort([3, 1, 2, 1, 3, 2, 1]) == [1, 1, 1, 2, 2, 3, 3]
 
