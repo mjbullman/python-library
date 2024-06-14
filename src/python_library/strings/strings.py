07b40a52f4efd13591dd036112ee95dec47803a2
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-14 22:27:42 +0300

    Find sub string implementation

--- src/python_library/strings/strings.py
+++ src/python_library/strings/strings.py
@@ -11,6 +11,28 @@ def reverse(string: str) -> str:
     return string[::-1]
 
 
+def find_substring(haystack: str, needle: str) -> int:
+    """
+   Returns the index of the first occurrence of needle in haystack,
+   or -1 if needle is not part of haystack.
+
+   Parameters:
+   haystack (str): The string to be searched.
+   needle (str): The substring to search for.
+
+   Returns:
+   int: The starting index of needle in haystack, or -1 if needle is not found.
+   """
+    len_needle: int = len(needle)
+    len_haystack: int = len(haystack)
+
+    for i in range(len_haystack - len_needle + 1):
+        if haystack[i:i + len_needle] == needle:
+            return i
+
+    return -1
+
+
 def length_of_longest_substring(string: str) -> int:
     """
    Finds the length of the longest substring without repeating characters.
@@ -23,7 +45,7 @@ def length_of_longest_substring(string: str) -> int:
    """
     char_index_map: dict = {}
     max_length: int = 0
-    start:int = 0
+    start: int = 0
 
     for end, char in enumerate(string):
         if char in char_index_map and char_index_map[char] >= start:
