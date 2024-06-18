824e3a9d96ed0fa7b75d982a61536ba9eb2848c9
Martin Bullman <martin.j.bullman@gmail.com>
2024-06-18 21:09:28 +0300

    Merge sort algorithm implementation

--- src/python_library/sorting_algorithms/sorting.py
+++ src/python_library/sorting_algorithms/sorting.py
@@ -1,3 +1,56 @@
+"""
+sorting_algorithms.py
+
+This module contains implementations of various sorting algorithms.
+Each algorithm sorts a list of elements in ascending order.
+
+Available sorting algorithms:
+- Bubble Sort
+- Selection Sort
+- Insertion Sort
+- Merge Sort
+- Quick Sort
+- Heap Sort
+
+Functions:
+- bubble_sort(arr): Sorts `arr` using the bubble sort algorithm.
+- selection_sort(arr): Sorts `arr` using the selection sort algorithm.
+- insertion_sort(arr): Sorts `arr` using the insertion sort algorithm.
+- merge_sort(arr): Sorts `arr` using the merge sort algorithm.
+- quick_sort(arr): Sorts `arr` using the quick sort algorithm.
+- heap_sort(arr): Sorts `arr` using the heap sort algorithm.
+
+Examples:
+    >>> from src.python_library.sorting_algorithms.sorting import bubble_sort
+    >>> arr = [64, 34, 25, 12, 22, 11, 90]
+    >>> bubble_sort(arr)
+    >>> print(arr)
+    [11, 12, 22, 25, 34, 64, 90]
+
+    >>> from src.python_library.sorting_algorithms.sorting import quick_sort
+    >>> arr = [10, 7, 8, 9, 1, 5]
+    >>> quick_sort(arr)
+    >>> print(arr)
+    [1, 5, 7, 8, 9, 10]
+
+Note:
+- All sorting functions modify the input list in place.
+- Ensure the input list is mutable and supports comparison operations.
+- For large datasets, prefer algorithms with better time complexity such as merge sort or quick
+  sort.
+
+References:
+- Cormen, Thomas H., et al. "Introduction to Algorithms." MIT Press, 2009.
+- Knuth, Donald E. "The Art of Computer Programming." Addison-Wesley, 1997.
+
+Author:
+- Martin Bullman
+
+Date:
+- 2024-06-14
+"""
+
+
 def bubble_sort(array: list) -> list:
     """
     Sorts a list of integers in ascending order using the bubble sort algorithm.
@@ -18,4 +71,130 @@ def bubble_sort(array: list) -> list:
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
 
+    return array
+
+
+def selection_sort(array: list) -> list:
+    """
+    Sorts an array using the insertion sort algorithm.
+
+    Parameters:
+    array (list): A list of elements to be sorted.
+
+    Returns:
+    list: The sorted list.
+    """
+    size: int = len(array)
+
+    for i in range(size):
+        min_index: int = i
+
+        for j in range(i + 1, size):
+            if array[j] < array[min_index]:
+                min_index = j
+
+        if array[min_index] < array[i]:
+            array[i], array[min_index] = array[min_index], array[i]
+
+    return array
+
+
+def insertion_sort(array: list) -> list:
+    """
+    Sorts an array using the insertion sort algorithm.
+
+    Parameters:
+    array (list): A list of elements to be sorted.
+
+    Returns:
+    list: The sorted list.
+    """
+    arr_len: int = len(array)
+
+    for i in range(1, arr_len):
+        key = array[i]
+        j = i - 1
+
+        while j >= 0 and array[j] > key:
+            array[j + 1] = array[j]
+            j -= 1
+
+        array[j + 1] = key
+
+    return array
+
+
+def merge(array: list, start: int, mid: int, end: int):
+    """
+    Merge two sorted subarrays of `array`.
+
+    The first subarray is array[start:mid+1].
+    The second subarray is array[mid+1:end+1].
+
+    Parameters:
+    array (list): The original array containing the subarrays to merge.
+    start (int): The starting index of the first subarray.
+    mid (int): The ending index of the first subarray and mid-point of the merge operation.
+    end (int): The ending index of the second subarray.
+    """
+    left_len = mid - start + 1
+    right_len = end - mid
+
+    L: list = array[start:mid + 1]
+    R : list = array[mid + 1:end + 1]
+
+    i: int = 0
+    j: int = 0
+    k: int = start
+
+    while i < left_len and j < right_len:
+        if L[i] <= R[j]:
+            array[k] = L[i]
+            i += 1
+        else:
+            array[k] = R[j]
+            j += 1
+
+        k += 1
+
+
+    while i < left_len:
+        array[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < right_len:
+        array[k] = R[j]
+        j += 1
+        k += 1
+
+
+def merge_sort(array: list, start: int, end: int) -> None | list:
+    """
+    Sort the array using the merge sort algorithm.
+
+    Parameters:
+    array (list): The array to be sorted.
+    start (int): The starting index of the subarray to be sorted.
+    end (int): The ending index of the subarray to be sorted.
+
+    Returns:
+    list: The sorted array.
+    """
+    if start >= end or len(array) == 0:
+        return array
+
+    mid = (start + end) // 2
+    merge_sort(array, start, mid)
+    merge_sort(array, mid + 1, end)
+    merge(array, start, mid, end)
+
+    return array
+
+
+def quick_sort(array: list) -> list:
+    return array
+
+
+def heap_sort(array: list) -> list:
     return array
--- tests/sorting_algorithms_tests/test_sorting_algorithms.py
+++ tests/sorting_algorithms_tests/test_sorting_algorithms.py
@@ -1,4 +1,4 @@
-from src.python_library.sorting_algorithms.sorting import bubble_sort
+from src.python_library.sorting_algorithms.sorting import *
 
 
 class TestBubbleSortAlgorithm:
@@ -20,3 +20,115 @@ class TestBubbleSortAlgorithm:
     def test_list_with_duplicates(self):
         assert bubble_sort([3, 1, 2, 1, 3, 2, 1]) == [1, 1, 1, 2, 2, 3, 3]
 
+    def test_large_array(self):
+        large_array = list(range(10000, 0, -1))
+        sorted_large_array = list(range(1, 10001))
+        assert bubble_sort(large_array) == sorted_large_array
+
+
+class TestSelectionSort:
+    def test_general_case(self):
+        assert selection_sort([64, 25, 12, 22, 11]) == [11, 12, 22, 25, 64]
+
+    def test_unsorted_list(self):
+        assert selection_sort([5, 3, 8, 6, 2]) == [2, 3, 5, 6, 8]
+
+    def test_empty_list(self):
+        assert selection_sort([]) == []
+
+    def test_single_element(self):
+        assert selection_sort([1]) == [1]
+
+    def test_two_elements(self):
+        assert selection_sort([2, 1]) == [1, 2]
+
+    def test_reverse_sorted(self):
+        assert selection_sort([3, 2, 1]) == [1, 2, 3]
+
+    def test_negative_numbers(self):
+        assert selection_sort([-2, -5, -1, -3]) == [-5, -3, -2, -1]
+
+    def test_all_same_elements(self):
+        assert selection_sort([1, 1, 1, 1]) == [1, 1, 1, 1]
+
+    def test_mixed_positive_negative(self):
+        assert selection_sort([3, -1, 0, -5, 8, 7]) == [-5, -1, 0, 3, 7, 8]
+
+    def test_floats(self):
+        assert selection_sort([3.2, 1.5, 4.8, 2.3]) == [1.5, 2.3, 3.2, 4.8]
+
+    def test_duplicates(self):
+        assert selection_sort([5, 1, 5, 3, 2]) == [1, 2, 3, 5, 5]
+
+    def test_large_array(self):
+        large_array = list(range(1000, 0, -1))
+        sorted_large_array = list(range(1, 1001))
+        assert selection_sort(large_array) == sorted_large_array
+
+
+class TestInsertionSort:
+    def test_general_case(self):
+        assert insertion_sort([64, 25, 12, 22, 11]) == [11, 12, 22, 25, 64]
+
+    def test_unsorted_list(self):
+        assert insertion_sort([5, 3, 8, 6, 2]) == [2, 3, 5, 6, 8]
+
+    def test_empty_list(self):
+        assert insertion_sort([]) == []
+
+    def test_single_element(self):
+        assert insertion_sort([1]) == [1]
+
+    def test_two_elements(self):
+        assert insertion_sort([2, 1]) == [1, 2]
+
+    def test_reverse_sorted(self):
+        assert insertion_sort([3, 2, 1]) == [1, 2, 3]
+
+    def test_negative_numbers(self):
+        assert insertion_sort([-2, -5, -1, -3]) == [-5, -3, -2, -1]
+
+    def test_all_same_elements(self):
+        assert insertion_sort([1, 1, 1, 1]) == [1, 1, 1, 1]
+
+    def test_mixed_positive_negative(self):
+        assert insertion_sort([3, -1, 0, -5, 8, 7]) == [-5, -1, 0, 3, 7, 8]
+
+    def test_floats(self):
+        assert insertion_sort([3.2, 1.5, 4.8, 2.3]) == [1.5, 2.3, 3.2, 4.8]
+
+    def test_duplicates(self):
+        assert insertion_sort([5, 1, 5, 3, 2, 6]) == [1, 2, 3, 5, 5, 6]
+
+    def test_large_array(self):
+        large_array = list(range(1000, 0, -1))
+        sorted_large_array = list(range(1, 1001))
+        assert insertion_sort(large_array) == sorted_large_array
+
+
+class TestMergeSort:
+    def test_empty_array(self):
+        assert merge_sort([], 0, 0) == []
+
+    def test_single_element_array(self):
+        assert merge_sort([1], 0, 0) == [1]
+
+    def test_sorted_array(self):
+        assert merge_sort([1, 2, 3, 4, 5], 0, 4) == [1, 2, 3, 4, 5]
+
+    def test_reverse_sorted_array(self):
+        assert merge_sort([5, 4, 3, 2, 1], 0, 4) == [1, 2, 3, 4, 5]
+
+    def test_random_array(self):
+        assert merge_sort([38, 27, 43, 3, 9, 82, 10], 0, 6) == [3, 9, 10, 27, 38, 43, 82]
+
+    def test_duplicate_elements(self):
+        assert merge_sort([4, 1, 3, 4, 2, 1, 5, 2], 0, 7) == [1, 1, 2, 2, 3, 4, 4, 5]
+
+    def test_negative_elements(self):
+        assert merge_sort([-3, -1, -7, -4, -5, -2], 0, 5) == [-7, -5, -4, -3, -2, -1]
+
+    def test_large_array(self):
+        large_array = list(range(1000, 0, -1))
+        sorted_large_array = list(range(1, 1001))
+        assert merge_sort(large_array, 0, len(large_array) - 1) == sorted_large_array
