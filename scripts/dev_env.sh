11a8e8aba73a7201ab391cc52fc2c3c89f5808c1
Martin Bullman <martin.j.bullman@gmail.com>
2025-12-29 17:50:52 +0200

    Updates the dev_env script to start up the environment with tmux, nvim, claude and lazygit

--- scripts/dev_env.sh
+++ scripts/dev_env.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
 SESSION_NAME="MJBPythonLibrary"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+VENV_PATH="$SCRIPT_DIR/../venv/bin/activate"
 
 if tmux has-session -t "${SESSION_NAME}" 2>/dev/null; then
-    echo "Session ${SESSION_NAME} already exist"
+    echo "Attaching to existing session: ${SESSION_NAME}..."
     tmux attach -t "${SESSION_NAME}"
-else 
-    echo "Session ${SESSION_NAME} does not exist"
-    tmux new-session -d -s "${SESSION_NAME}"
-    tmux new-window -t "nvim"
-    tmux new-window -t "claude"
-    tmux new-window -t "lazygit"
+else
+    echo "Creating new session: ${SESSION_NAME}..."
+
+    # create new session and start nvim
+    tmux new-session -d -s "${SESSION_NAME}" -n nvim nvim
+
+    sleep 1
+    # split nvim window horizontally (top/bottom)
+    tmux split-window -v -t "${SESSION_NAME}"
+
+    # create new windows and run cluade code
+    sleep 1
+    tmux new-window -t "${SESSION_NAME}" claude
+    
+    # create new windows and run lazygit
+    sleep 1
+    tmux new-window -t "${SESSION_NAME}" lazygit
+
+    # focus nvim window
+    tmux select-window -t "${SESSION_NAME}:nvim"
+
+    # attache to session
+    tmux attach -t "${SESSION_NAME}"
 fi
 
 echo "Martin Bullman"
+
